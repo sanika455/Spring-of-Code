f7f1109ecfef7e622225f1b42c2c8ad36c7898e7
Piyusha Ghodake <105534754+PiyushaGhodake@users.noreply.github.com>
2023-03-22 18:17:37 +0530

    Update ValidSudoku_Solution.java

--- Solutions/ValidSudoku_Solution.java
+++ Solutions/ValidSudoku_Solution.java
@@ -1,7 +1,8 @@
+import java.util.*;
 public class Solution {
 
     public static boolean isValidSudoku(char[][] board) {
-        HashSet<String> hs = new HashSet();
+        HashSet<String> hs = new HashSet<>();
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board[0].length; j++) {
                 char ch = board[i][j];
@@ -17,4 +18,24 @@ public class Solution {
         }
         return true;
     }
+    public static void main(String args[])
+    {
+    	
+    	 char[][] board = new char[][] {
+             { '5', '3', '.', '.', '7', '.', '.', '.', '.' },
+             { '6', '.', '.', '1', '9', '5', '.', '.', '.' },
+             { '.', '9', '8', '.', '.', '.', '.', '6', '.' },
+             { '8', '.', '.', '.', '6', '.', '.', '.', '3' },
+             { '4', '.', '.', '8', '.', '3', '.', '.', '1' },
+             { '7', '.', '.', '.', '2', '.', '.', '.', '6' },
+             { '.', '6', '.', '.', '.', '.', '2', '8', '.' },
+             { '.', '.', '.', '4', '1', '9', '.', '.', '5' },
+             { '.', '.', '.', '.', '8', '.', '.', '7', '9' }
+         };
+ 
+      System.out.println((isValidSudoku(board) ? "YES" : "NO"));
+         
+    }
+    
 }
+
